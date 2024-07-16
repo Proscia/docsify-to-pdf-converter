340307dec3691920059add7661b6595c8f73f16a
Ben Memberg <benmemberg@gmail.com>
2024-07-16 16:11:36 -0400

    Write/remove static directory within the docsify root

--- src/render.js
+++ src/render.js
@@ -53,8 +53,9 @@ const htmlToPdf = ({
   removeTemp,
   docsifyRendererPort,
   emulateMedia,
+  pathToDocsifyEntryPoint, 
 }) => async () => {
-  const { closeProcess } = require("./utils.js")({ pathToStatic, removeTemp });
+  const { closeProcess } = require("./utils.js")({ pathToDocsifyEntryPoint, pathToStatic, removeTemp });
   try {
     return await renderPdf({
       mainMdFilename,
